9aa51d7f532f0ca7c388c90c3d802e7558adb191
isalovesj <idmtz.p@gmail.com>
2016-09-15 11:25:58 -0400

    fixed

--- Assignments/Lesson02-Assignment01-Collections/student-start/module2_lesson2_formative.rb
+++ Assignments/Lesson02-Assignment01-Collections/student-start/module2_lesson2_formative.rb
@@ -1,12 +1,8 @@
 # Grab 23 random elements between 0 and 10000
-arr = (1..10000).to_a.sample(23)
+arr = (1..10000).to_a
 p arr
-
-# This selects only elements that when divided by 3 have a remainder of 0 
-# using the % (modulus) operator
-p arr.select { |element| element % 3 == 0 }
-
-# Using `reject` method filter out anything less than 5000
-# and use `sort` and `reverse` methods to sort in descending order
-# Start with the line below and continue as 1 long method chain
-# p arr.select { |element| element % 3 == 0 }
+new_arr=arr.select { |num| num % 3 == 0 }
+p new_arr
+new_arr=arr.select { |num| num % 3 == 0 }
+.reject{|num| num < 5000}
+p new_arr.sample(3).sort.reverse.inspect
